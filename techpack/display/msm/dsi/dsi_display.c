20d5a08aa2fe3f8ba2f4ac15c2288e39f49ec35a
LuK1337 <priv.luk@gmail.com>
2021-10-03 15:42:32 +0200

    techpack: display: Apply correct panel mode on setting backlight lvl
    This follows similar behavior present in OxygenOS kernel and fixes
    invalid panel mode after toggling the screen off and on again on 6T.
    
    Change-Id: Ia90a40b31c3956df9d8ce75b0fad82c9f4d8ef9d

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -215,6 +215,9 @@ int dsi_display_set_backlight(struct drm_connector *connector,
 		goto error;
 	}
 
+	if (bl_lvl != 0 && panel->bl_config.bl_level == 0)
+		dsi_panel_apply_display_mode_locked(panel);
+
 	panel->bl_config.bl_level = bl_lvl;
 
 	/* scale backlight */
