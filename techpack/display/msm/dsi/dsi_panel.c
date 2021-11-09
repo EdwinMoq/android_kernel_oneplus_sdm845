c8562140889a48c535899f569f6b286e005c8bd3
Art_Chen <Chenxy0201@qq.com>
2021-11-09 14:32:18 +0800

    techpack: display: Implement panel status check
    * to ensure dimlayer has been killed when the panel is off.
    
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Change-Id: I30606f5fb4fcd493a94b41792a75997c86ba4545

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -4123,6 +4123,7 @@ error:
 	return rc;
 }
 
+extern int oneplus_panel_status;
 int dsi_panel_set_lp1(struct dsi_panel *panel)
 {
 	int rc = 0;
@@ -4151,6 +4152,9 @@ int dsi_panel_set_lp1(struct dsi_panel *panel)
 	if (rc)
 		DSI_ERR("[%s] failed to send DSI_CMD_SET_LP1 cmd, rc=%d\n",
 		       panel->name, rc);
+
+	oneplus_panel_status = 3; // DISPLAY_POWER_DOZE
+
 exit:
 	mutex_unlock(&panel->panel_lock);
 	return rc;
@@ -4173,6 +4177,9 @@ int dsi_panel_set_lp2(struct dsi_panel *panel)
 	if (rc)
 		DSI_ERR("[%s] failed to send DSI_CMD_SET_LP2 cmd, rc=%d\n",
 		       panel->name, rc);
+
+	oneplus_panel_status = 4; // DISPLAY_POWER_DOZE_SUSPEND
+
 exit:
 	mutex_unlock(&panel->panel_lock);
 	return rc;
@@ -4203,6 +4210,9 @@ int dsi_panel_set_nolp(struct dsi_panel *panel)
 	if (rc)
 		DSI_ERR("[%s] failed to send DSI_CMD_SET_NOLP cmd, rc=%d\n",
 		       panel->name, rc);
+
+	oneplus_panel_status = 2; // DISPLAY_POWER_ON
+
 exit:
 	mutex_unlock(&panel->panel_lock);
 	return rc;
@@ -4544,6 +4554,7 @@ int dsi_panel_enable(struct dsi_panel *panel)
 	else
 		panel->panel_initialized = true;
 
+	oneplus_panel_status = 2; // DISPLAY_POWER_ON
 	oneplus_dimlayer_hbm_enable = backup_dimlayer_hbm;
 	oneplus_dim_status = backup_dim_status;
 	pr_err("Restore dim when panel goes on");
@@ -4666,6 +4677,7 @@ int dsi_panel_disable(struct dsi_panel *panel)
 			rc = 0;
 		}
 	}
+	oneplus_panel_status = 0; // DISPLAY_POWER_OFF
 	panel->panel_initialized = false;
 	panel->power_mode = SDE_MODE_DPMS_OFF;
 
@@ -4823,6 +4835,7 @@ int dsi_panel_set_aod_mode(struct dsi_panel *panel, int level)
 			aod_complete = false;
 		}
 	}
+	oneplus_panel_status = 0; // DISPLAY_POWER_OFF
 
 	panel->aod_curr_mode = level;
 	pr_err("AOD MODE = %d\n", level);
