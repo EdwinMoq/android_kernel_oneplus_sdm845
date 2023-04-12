f7d2f46dc2e49f52dc7d6d7c24cff9ab970d9c34
Edwin Moquete <edwinmmoquete@gmail.com>
2023-04-12 16:13:39 +0000

    iio: adc: Add OnePlus adc channels

--- drivers/iio/adc/qcom-spmi-adc5.c
+++ drivers/iio/adc/qcom-spmi-adc5.c
@@ -989,6 +989,10 @@ static const struct adc_channels adc_chans_rev2[ADC_MAX_CHANNEL] = {
 					SCALE_HW_CALIB_THERM_100K_PULLUP)
 	[ANA_IN]		= ADC_CHAN_TEMP("drax_temp", 1,
 					SCALE_HW_CALIB_PMIC_THERM)
+	[ADC_AMUX_THM4_PU2]	= ADC_CHAN_TEMP("pa1_therm", 1,
+					SCALE_HW_CALIB_THERM_100K_PULLUP)
+	[VADC_LR_MUX7_HW_ID]	= ADC_CHAN_VOLT("gpio12_v", 1,
+					SCALE_HW_CALIB_DEFAULT)
 };
 
 static int adc_get_dt_channel_data(struct adc_chip *adc,
