4c9ea5aa47a829da07680e451597df7bbcd91a2b
Karthik Anantha Ram <kartanan@codeaurora.org>
2025-10-15 10:17:20 +0500

    msm: camera: icp: Increase timeout for abort & destroy
    Due to scheduling delays there is a possibility that
    the abort/destroy ACK from FW cannot be processed within the
    current timeout. Hence increasing the timeout to account
    for these delays.
    
    Change-Id: Ia8f670e67dfb3db47e71b1d1e9eeba79bde7f277
    Signed-off-by: Karthik Anantha Ram <kartanan@codeaurora.org>

--- drivers/media/platform/msm/camera_oneplus/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c
+++ drivers/media/platform/msm/camera_oneplus/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c
@@ -2285,7 +2285,7 @@ static int cam_icp_mgr_abort_handle(
 	int rc = 0;
 	unsigned long rem_jiffies;
 	size_t packet_size;
-	int timeout = 100;
+	int timeout = 1000;
 	struct hfi_cmd_work_data *task_data;
 	struct hfi_cmd_ipebps_async *abort_cmd;
 	struct crm_workq_task *task;
@@ -2349,7 +2349,7 @@ static int cam_icp_mgr_destroy_handle(
 	struct cam_icp_hw_ctx_data *ctx_data)
 {
 	int rc = 0;
-	int timeout = 100;
+	int timeout = 1000;
 	unsigned long rem_jiffies;
 	size_t packet_size;
 	struct hfi_cmd_work_data *task_data;
